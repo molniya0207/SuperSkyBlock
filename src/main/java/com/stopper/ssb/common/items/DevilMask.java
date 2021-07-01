fbe74c05ba09e996232d882194d57cc429fa6229
stopper <molniya0207@gmail.com>
2021-07-01 21:14:49 +0600

    Devil Mask effects

---
+++ src/main/java/com/stopper/ssb/common/items/DevilMask.java
@@ -0,0 +1,50 @@
+package com.stopper.ssb.common.items;
+
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class DevilMask extends ArmorItem {
+    String g_name;
+    String g_tooltip;
+
+    public DevilMask(String name, String tooltip, IArmorMaterial mat, EquipmentSlotType slot, Properties props) {
+        super(mat, slot, props);
+        g_name = name;
+        g_tooltip = tooltip;
+    }
+
+    @Override
+    public int getDefense() {
+        return 4;
+    }
+
+    @Override
+    public void onArmorTick(ItemStack stack, World world, PlayerEntity player) {
+        super.onArmorTick(stack, world, player);
+        player.addEffect(new EffectInstance(Effects.MOVEMENT_SPEED, 100, 2));
+        player.addEffect(new EffectInstance(Effects.DAMAGE_BOOST, 100, 2));
+        player.addEffect(new EffectInstance(Effects.SATURATION, 100));
+    }
+
+    @Override
+    public ITextComponent getName(ItemStack stack) {
+        return new TranslationTextComponent(g_name);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable World worldIn, List<ITextComponent> list, ITooltipFlag flags) {
+        list.add(new TranslationTextComponent(g_tooltip));
+    }
+}
